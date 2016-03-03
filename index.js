b7d98ff3ed22bfd8ff9bbfd92ae880f33604e4e2
Eli Dupuis <elidupuis@gmail.com>
2016-03-02 17:28:34 -0700

    Add option to automatically generate a version file

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 /* jshint node: true */
 'use strict';
 
+var writeFile = require('broccoli-file-creator');
+
 module.exports = {
-  name: 'ember-cli-new-version'
+  name: 'ember-cli-new-version',
+
+  included: function(app, parentAddon) {
+    this.options = app.options.newVersion || {};
+
+    if (this.options === true) {
+      this.options = { fileName: 'VERSION.txt' };
+    }
+  },
+
+  treeForPublic: function() {
+    var content = this.parent.pkg.version || '';
+    var fileName = this.options.fileName;
+
+    if (this.options.fileName) {
+      this.ui.writeLine('Created ' + fileName);
+      return writeFile(fileName, content);
+    }
+  }
 };
