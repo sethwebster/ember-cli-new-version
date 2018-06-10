750bd52a376c2eca1028bbea4fb00c3072da5bb3
Blake Gentry <blakesgentry@gmail.com>
2018-06-10 13:55:07 -0700

    ember-cli-update to v3.1

--- testem.js
+++ testem.js
@@ -5,8 +5,21 @@ module.exports = {
   "launch_in_ci": [
     "PhantomJS"
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
