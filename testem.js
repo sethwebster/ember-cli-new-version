e17d8f2af444c574487eb5abdd8eb138618e2fc4
Blake Gentry <blakesgentry@gmail.com>
2018-06-10 15:12:36 -0700

    finalize test setup upgrades

--- testem.js
+++ testem.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  test_page: "tests/index.html?hidepassed",
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome'
   ],
   launch_in_dev: [
     'Chrome'
