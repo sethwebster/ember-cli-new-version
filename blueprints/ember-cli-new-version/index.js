78d97d5480b52a7c76165a2bc3600097923a7700
Ilya Radchenko <knownasilya@gmail.com>
2019-01-08 22:57:03 -0500

    fix: add ember-fetch via a default blueprint instead

---
+++ blueprints/ember-cli-new-version/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall() {
+    // Add addons to package.json and run defaultBlueprint
+    return this.addAddonToProject('ember-fetch');
+  }
+};
