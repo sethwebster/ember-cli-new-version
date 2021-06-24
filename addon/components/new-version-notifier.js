83c35280e847aa27a831334ff55ed29424396df4
Philippe Bernery <pbernery@users.noreply.github.com>
2021-06-24 18:53:04 +0200

    feat: Extract new version fetching and checking from the NewVersionNotifier component in a service (#94)
    BREAKING CHANGE: introduces a new version service. Some arguments from the components have been moved here and other moved to the configuration.
    BREAKING CHANGE: How the version check happens has also changed, see https://github.com/sethwebster/ember-cli-new-version/pull/94
    
    * Enable JS code checking
    
    * Extract new version fetching and checking from the NewVersionNotifier component in a service
    
    The logic is now in a service so that it can be used independently of the action.
    
    There are slight behaviour changes and new behaviours as well.
    
    The service contains an observable `isNewVersionAvailable` property.
    This property is set to true when a new version is available.
    
    The version checking is slightly different: before, the first check was ignored because the component was comparing the previously fetched version and the latest fetched version.
    Now, the version is always checked against the version provided in environment.js. This thus means the comparison is always done against the compiled version of the app.
    
    The component had a way to ignore a version, by setting the next version to check to the latest fetched version. To keep the compatibility with this behaviour, there is now an explicit list of "ignore versions" in the service, populated when the user closes the `NewVersionNotifier`. Note that this list is kept in memory only and when the user reloads the page, the list is empty again. This is the same behaviour as before.
    
    I kept the code related to testing in the service to limit the changes, but this could be moved in the TestNewVersionService implementation so that the original NewVersionService is kept clean of testing behaviour.
    
    The onNewVersion action still exists but is now in the service. If one needs this callback, he/she can overload the NewVersionService and override onNewVersion. Same thing for onError.
    
    Some properties that were arguments of the component have been moved to the configuration.
    
    * Fix dependencies declaration
    
    Some addon dependencies were declared in devDependencies while they are needed by the addon to run.

--- addon/components/new-version-notifier.js
+++ addon/components/new-version-notifier.js
@@ -1,152 +1,37 @@
-import { getOwner } from '@ember/application';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import Ember from 'ember';
-import { task, timeout } from 'ember-concurrency';
-import fetch from 'fetch';
-import { later } from '@ember/runloop';
-import { waitFor } from '@ember/test-waiters';
-
-let taskRunCounter = 0;
-
-const MAX_COUNT_IN_TESTING = 10;
-const ONE_MINUTE = 60000;
+import { inject as service } from '@ember/service';
 
 export default class NewVersionNotifier extends Component {
-  @tracked enableInTests = this.args.enableInTests ?? false;
-  @tracked updateInterval = this.args.updateInterval ?? ONE_MINUTE;
-  @tracked versionFileName = this.args.versionFileName ?? '/VERSION.txt';
-  @tracked firstCheckInterval = this.args.firstCheckInterval ?? 0;
+  /** @type {import("ember-cli-new-version/services/new-version").default} */
+  @service newVersion;
+
   @tracked updateMessage =
     this.args.updateMessage ??
     'This application has been updated from version {oldVersion} to {newVersion}. Please save any work, then refresh browser to see changes.';
   @tracked showReload = this.args.showReload ?? true;
   @tracked reloadButtonText = this.args.reloadButtonText ?? 'Reload';
-  @tracked onNewVersion = this.args.onNewVersion
-    ? this.args.onNewVersion
-    : () => {};
-  @tracked onError = this.args.onError
-    ? this.args.onError
-    : (e) => {
-        if (!Ember.testing) {
-          console.log(e);
-        }
-      };
 
-  // internal state:
-  @tracked message = '';
-  @tracked lastVersion = null;
-  @tracked version = null;
-
-  get _fastboot() {
-    let owner = getOwner(this);
-    return owner.lookup('service:fastboot');
-  }
-
-  get url() {
-    const config = getOwner(this).resolveRegistration('config:environment');
-    const versionFileName = config.newVersion?.fileName || this.versionFileName;
-    const baseUrl =
-      config.newVersion?.prepend || config.rootURL || config.baseURL;
-
-    if (!config || baseUrl === '/') {
-      return versionFileName;
+  /**
+   * @returns {string | undefined}
+   */
+  get message() {
+    if (this.newVersion.isNewVersionAvailable) {
+      return this.updateMessage
+        .replace('{oldVersion}', this.newVersion.currentVersion)
+        .replace('{newVersion}', this.newVersion.latestVersion);
     }
 
-    return baseUrl + versionFileName;
+    return undefined;
   }
 
-  constructor() {
-    super(...arguments);
-
-    if (this._fastboot?.isFastBoot) {
-      return;
-    }
-
-    if (Ember.testing) {
-      taskRunCounter = 0;
-    }
-
-    if (!Ember.testing || this.enableInTests) {
-      if (this.firstCheckInterval > 0) {
-        later(
-          this,
-          () => {
-            this.updateVersion.perform();
-          },
-          this.firstCheckInterval
-        );
-      } else {
-        this.updateVersion.perform();
-      }
-    }
-  }
-
-  get updateIntervalWithTesting() {
-    let enableInTests = this.enableInTests;
-    return !enableInTests && Ember.testing ? 0 : this.updateInterval;
-  }
-
-  @task
-  @waitFor
-  *updateVersion() {
-    const url = this.url;
-
-    try {
-      yield fetch(url + '?_=' + Date.now())
-        .then((response) => {
-          if (!response.ok) throw new Error(response.statusText);
-          return response.text();
-        })
-        .then((res) => {
-          const currentVersion = this.version;
-          const newVersion = res && res.trim();
-
-          if (this.updateNeeded(currentVersion, newVersion)) {
-            const message = this.updateMessage
-              .replace('{oldVersion}', currentVersion)
-              .replace('{newVersion}', newVersion);
-
-            this.message = message;
-            this.lastVersion = currentVersion;
-
-            this.onNewVersion(newVersion, currentVersion);
-          }
-
-          this.version = newVersion;
-        });
-    } catch (e) {
-      this.onError(e);
-    } finally {
-      let updateInterval = this.updateIntervalWithTesting;
-      if (updateInterval === null || updateInterval === undefined) {
-        updateInterval = ONE_MINUTE;
-      }
-
-      yield timeout(updateInterval);
-
-      if (Ember.testing && ++taskRunCounter > MAX_COUNT_IN_TESTING) {
-        return;
-      }
-
-      if (Ember.testing && !this.enableInTests) {
-        return;
-      }
-      this.updateVersion.perform();
-    }
-  }
-
-  updateNeeded(currentVersion, newVersion) {
-    return currentVersion && newVersion !== currentVersion;
+  close() {
+    this.newVersion.ignoreVersion(this.newVersion.latestVersion);
   }
 
   reload() {
     if (typeof window !== 'undefined' && window.location) {
       window.location.reload(true);
     }
-  }
-
-  close() {
-    this.message = undefined;
   }
 }
