8dfa50c58b63a24faeab6e368e7c2b7c364c97fe
Philippe Bernery <pbernery@users.noreply.github.com>
2021-06-24 18:53:04 +0200

    feat: Extract new version fetching and checking from the NewVersionNotifier component in a service (#94)
    BREAKING CHANGE: introduces a new version service. Some arguments from the components have been moved here and other moved to the configuration.
    BREAKING CHANGE: How the version check happens has also changed, see https://github.com/sethwebster/ember-cli-new-version/pull/94
    
    * Enable JS code checking
    
    * Extract new version fetching and checking from the NewVersionNotifier component in a service
    
    The logic is now in a service so that it can be used independently of the action.
    
    There are slight behaviour changes and new behaviours as well.
    
    The service contains an observable `isNewVersionAvailable` property.
    This property is set to true when a new version is available.
    
    The version checking is slightly different: before, the first check was ignored because the component was comparing the previously fetched version and the latest fetched version.
    Now, the version is always checked against the version provided in environment.js. This thus means the comparison is always done against the compiled version of the app.
    
    The component had a way to ignore a version, by setting the next version to check to the latest fetched version. To keep the compatibility with this behaviour, there is now an explicit list of "ignore versions" in the service, populated when the user closes the `NewVersionNotifier`. Note that this list is kept in memory only and when the user reloads the page, the list is empty again. This is the same behaviour as before.
    
    I kept the code related to testing in the service to limit the changes, but this could be moved in the TestNewVersionService implementation so that the original NewVersionService is kept clean of testing behaviour.
    
    The onNewVersion action still exists but is now in the service. If one needs this callback, he/she can overload the NewVersionService and override onNewVersion. Same thing for onError.
    
    Some properties that were arguments of the component have been moved to the configuration.
    
    * Fix dependencies declaration
    
    Some addon dependencies were declared in devDependencies while they are needed by the addon to run.

--- tests/integration/components/new-version-notifier-test.js
+++ tests/integration/components/new-version-notifier-test.js
@@ -1,14 +1,48 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render, waitUntil } from '@ember/test-helpers';
+import NewVersionService from 'ember-cli-new-version/services/new-version';
+import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
 import setupMirage from '../../helpers/setup-mirage';
-import Mirage from 'ember-cli-mirage';
-import { set } from '@ember/object';
+
+class TestNewVersionService extends NewVersionService {
+  get _newVersionConfig() {
+    const config = super._newVersionConfig;
+    config.enableInTests = true;
+    config.updateInterval = 100;
+    config.currentVersion = 'v1.0.1';
+    return config;
+  }
+}
 
 module('Integration | Component | new version notifier', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    this.owner.register('service:new-version', TestNewVersionService);
+  });
+
+  test('it shows when a new version is available', async function (assert) {
+    let called = false;
+
+    this.server.get('/VERSION.txt', function () {
+      setTimeout(() => {
+        called = true;
+      }, 20);
+      return 'v1.0.2';
+    });
+
+    render(hbs`<NewVersionNotifier />`);
+
+    await waitUntil(() => called, { timeout: 50 });
+
+    assert
+      .dom('.update-notification')
+      .containsText(
+        'This application has been updated from version v1.0.1 to v1.0.2. Please save any work, then refresh browser to see changes.'
+      );
+  });
 
   test('it yields the current and last version to the block', async function (assert) {
     assert.expect(9);
@@ -26,10 +60,10 @@ module('Integration | Component | new version notifier', function (hooks) {
     });
 
     render(hbs`
-      {{#new-version-notifier updateInterval=100 enableInTests=true as |version lastVersion reload close|}}
+      <NewVersionNotifier as |version lastVersion|>
         <div id="version-value">{{version}}</div>
         <div id="last-version-value">{{lastVersion}}</div>
-      {{/new-version-notifier}}
+      </NewVersionNotifier>
     `);
 
     await waitUntil(() => called, { timeout: 50 });
@@ -53,187 +87,6 @@ module('Integration | Component | new version notifier', function (hooks) {
     await waitUntil(() => called, { timeout: 250 });
     assert.equal(callCount, 3);
     assert.dom('#version-value').hasText('v1.0.3');
-    assert.dom('#last-version-value').hasText('v1.0.2');
-  });
-
-  test('it calls onNewVersion when a new version is detected', async function (assert) {
-    assert.expect(4);
-    let done = assert.async(2);
-
-    let callCount = 0;
-
-    this.server.get('/VERSION.txt', function () {
-      ++callCount;
-      return `v1.0.${callCount}`;
-    });
-
-    set(this, 'onNewVersion', (newVersion, oldVersion) => {
-      assert.equal(
-        newVersion,
-        'v1.0.2',
-        'newVersion v1.0.2 is sent to onNewVersion'
-      );
-      assert.equal(
-        oldVersion,
-        'v1.0.1',
-        'oldVersion v1.0.1 is sent to onNewVersion'
-      );
-      done();
-    });
-    set(this, 'enableInTests', true);
-
-    render(
-      hbs`{{new-version-notifier updateInterval=100 enableInTests=enableInTests onNewVersion=onNewVersion}}`
-    );
-
-    await waitUntil(() => callCount === 1, { timeout: 95 });
-    assert.equal(callCount, 1, '1 call was made');
-
-    await waitUntil(() => callCount === 2, { timeout: 190 });
-    assert.equal(callCount, 2);
-    set(this, 'enableInTests', false); // stop the loop from continuing
-    done();
-  });
-
-  test('one version', async function (assert) {
-    assert.expect(2);
-
-    let callCount = 0;
-
-    this.server.get('/VERSION.txt', function () {
-      ++callCount;
-      return 'v1.0.3';
-    });
-
-    set(this, 'onNewVersion', (newVersion, oldVersion) => {
-      throw `unexpected call to onNewVersion with ${newVersion}, ${oldVersion}`;
-    });
-
-    render(
-      hbs`{{new-version-notifier updateInterval=100 enableInTests=true onNewVersion=onNewVersion}}`
-    );
-
-    await waitUntil(() => callCount === 4, { timeout: 490 });
-    assert.dom(document.querySelector('#ember-testing-container')).hasText('');
-    assert.equal(callCount, 4);
-  });
-
-  test('repeat on bad response', async function (assert) {
-    assert.expect(2);
-
-    let callCount = 0;
-
-    this.server.get('/VERSION.txt', function () {
-      ++callCount;
-
-      if (callCount === 1) {
-        return new Mirage.Response(500, {}, { message: '' });
-      }
-
-      return 'v1.0.3';
-    });
-
-    set(this, 'onNewVersion', (newVersion, oldVersion) => {
-      throw `unexpected call to onNewVersion with ${newVersion}, ${oldVersion}`;
-    });
-
-    render(
-      hbs`{{new-version-notifier updateInterval=100 enableInTests=true onNewVersion=onNewVersion}}`
-    );
-
-    await waitUntil(() => callCount === 4, { timeout: 490 });
-    assert.dom(document.querySelector('#ember-testing-container')).hasText('');
-    assert.equal(callCount, 4);
-  });
-
-  test('it calls onError when request fails', async function (assert) {
-    assert.expect(1);
-
-    let called = false;
-
-    this.server.get('/VERSION.txt', function () {
-      setTimeout(() => {
-        called = true;
-      }, 100);
-      return new Mirage.Response(500, {}, { message: '' });
-    });
-
-    let onErrorCalled = false;
-    set(this, 'onError', () => {
-      onErrorCalled = true;
-    });
-
-    render(
-      hbs`{{new-version-notifier updateInterval=100 enableInTests=true onError=onError}}`
-    );
-
-    await waitUntil(() => called, { timeout: 150 });
-    assert.ok(onErrorCalled, 'onError was called');
-  });
-
-  test('it accepts a custom updateNeeded function', async function (assert) {
-    assert.expect(5);
-    let done = assert.async(2);
-
-    let callCount = 0;
-
-    this.server.get('/VERSION.txt', function () {
-      ++callCount;
-      switch (callCount) {
-        case 0:
-          return 'v1.0.0';
-        case 1:
-          return 'v1.0.1';
-        case 2:
-          return 'v1.0.2';
-        case 3:
-          return 'v1.1.0';
-        default:
-          return null;
-      }
-    });
-
-    set(this, 'onNewVersion', (newVersion, oldVersion) => {
-      assert.equal(
-        newVersion,
-        'v1.1.0',
-        'newVersion v1.1.0 is sent to onNewVersion'
-      );
-      assert.equal(
-        oldVersion,
-        'v1.0.2',
-        'oldVersion v1.0.2 is sent to onNewVersion'
-      );
-      done();
-    });
-    set(this, 'enableInTests', true);
-
-    set(this, 'updateNeeded', function (currentVersion, newVersion) {
-      if (!currentVersion) {
-        return false;
-      }
-
-      // Only compare major and minor version number
-      let currentV = currentVersion.substr(0, currentVersion.lastIndexOf('.'));
-      let newV = newVersion.substr(0, newVersion.lastIndexOf('.'));
-
-      return newV !== currentV;
-    });
-
-    render(
-      hbs`{{new-version-notifier updateNeeded=updateNeeded updateInterval=100 enableInTests=enableInTests onNewVersion=onNewVersion}}`
-    );
-
-    await waitUntil(() => callCount === 1, { timeout: 95 });
-    assert.equal(callCount, 1, '1 call was made');
-
-    await waitUntil(() => callCount === 2, { timeout: 190 });
-    assert.equal(callCount, 2);
-
-    await waitUntil(() => callCount === 3, { timeout: 190 });
-    assert.equal(callCount, 3);
-
-    set(this, 'enableInTests', false); // stop the loop from continuing
-    done();
+    assert.dom('#last-version-value').hasText('v1.0.1');
   });
 });
