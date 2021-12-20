0a52097a63f90500e7756d609ffb79f64e17727c
Bas Kamer <bas@bushbaby.nl>
2021-12-20 14:17:24 +0100

    fix: cs

--- addon/services/new-version.js
+++ addon/services/new-version.js
@@ -1,5 +1,4 @@
 import { getOwner } from '@ember/application';
-import { A } from '@ember/array';
 import { later } from '@ember/runloop';
 import Service from '@ember/service';
 import { waitFor } from '@ember/test-waiters';
@@ -128,7 +127,8 @@ export default class NewVersionService extends Service {
           if (this.isNewVersionAvailable) {
             this.onNewVersion(
               this.latestVersion,
-              this.ignoredVersions[this.ignoredVersions.length - 1] || this.currentVersion
+              this.ignoredVersions[this.ignoredVersions.length - 1] ||
+                this.currentVersion
             );
           }
         });
